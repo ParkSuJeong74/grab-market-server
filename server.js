459a646b885747a194a52c5558928284086e5b41
ParkSuJeong <evergreen9158@naver.com>
2022-01-05 18:00:49 +0900

    upload 구현

--- server.js
+++ server.js
@@ -20,6 +20,20 @@ app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
+app.get("/uploads", (req, res) => {
+    models.Banner.findAll({
+        limit:2
+    }).then((result) => {
+        res.send({
+            banner : result,
+        })
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).send("배너에 문제가 발생했습니다.");
+    })
+})
+
+
 app.get("/products", (req, res) => {
     models.Product.findAll({
         limit: 50,
@@ -40,16 +54,16 @@ app.get("/products", (req, res) => {
         })
     }).catch((error) => {
         console.error(error);
-        res.send("조회에 문제가 발생했습니다.");
+        res.status(400).send("조회에 문제가 발생했습니다.");
     })
     
 });
 app.post("/products", (req, res) => {
     const body = req.body;
-    const {name, price, seller, description} = body;
+    const {name, price, seller, description, imageUrl} = body;
 
-    if(!name || !price || !seller || !description){
-        res.send("모든 필드를 입력해주세요.")
+    if(!name || !price || !seller || !description || !imageUrl){
+        res.status(400).send("모든 필드를 입력해주세요.")
         return;
     }
 
@@ -57,7 +71,8 @@ app.post("/products", (req, res) => {
         name,
         price, 
         seller,
-        description
+        description,
+        imageUrl
     })
     .then((result) => {
         console.log("업로드 완료 : ", result);
@@ -67,7 +82,7 @@ app.post("/products", (req, res) => {
     })
     .catch((error) => {
         console.error(error);
-        res.send("업로드에 문제가 발생했습니다.");
+        res.status(400).send("업로드에 문제가 발생했습니다.");
     })
 });
 app.get("/products/:id", (req, res) => {
@@ -87,7 +102,7 @@ app.get("/products/:id", (req, res) => {
     })
     .catch((error) => {
         console.error(error);
-        res.send("상세 페이지 조회에 문제가 발생했습니다.");
+        res.status(400).send("상세 페이지 조회에 문제가 발생했습니다.");
     })
 })
 
